78d6c1f51e8fc778caa98b0f708d4f1699ed8aee
febri0312 <164834459+febri0312@users.noreply.github.com>
2024-03-25 11:07:57 +0700

    Add files via upload

---
+++ ukk_Opsi1/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Website Galeri Foto</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-danger-subtle bg-danger-subtle">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Website Galeri Foto</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+                <div class="navbar-nav me-auto">
+                </div>
+                <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+                <a href="login.php" class="btn btn-outline-success m-1">Masuk</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row justify-content-center text-center">
+            <div class="card mb-3">
+                <div class="card text-bg-dark">
+                    <img src="assets/img/yy.jpg" class="card-img" alt="memories">
+                    <div class="card-img-overlay">
+                        <h1 class="card-title text-center"> Gallery's </h1>
+                        <h3 class="card-text text-center">Febriani Mustika</p>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    </div>
+
+
+</body>
+
+
+<footer class="d-flex justify-content-center border-top mt-3 bg-light">
+    <p>&copy; UKK RPL 2024 | Febriani Mustika</p>
+</footer>
+<script type="text/javascript" src="assets/css/bootstrap.min.js"></script>
+</body>
+
+</html>
